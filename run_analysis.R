b0508c3ca78c4c6a9dbc0a711685590b1e5cf321
Jay D. McHugh <jaydmchugh@gmail.com>
2015-01-15 11:52:29 -0600

    Removed a couple of additional unnecessary variables.

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,3 @@
-library(stringr)
-library(plyr)
 library(dplyr)
 library(data.table)
 library(LaF)
@@ -17,6 +15,8 @@ train_dir
 feature.file <- paste0(meta_dir, "features.txt")
 features <- read.table(feature.file, sep = " ", col.names = c("feature_id", "feature_name"), colClasses=c("numeric","character"))
 
+rm("feature.file")
+
 pretty_features <- mutate(features, feature_name = gsub(",","-",gsub("\\)","",gsub("\\(","",features$feature_name))))
 
 features <- data.table(features)
@@ -26,6 +26,8 @@ head(features)
 
 activity.file <- paste0(meta_dir, "activity_labels.txt")
 activities <- read.table(activity.file, sep = " ", col.names = c("activity_id", "activity_name"))
+
+rm("activity.file")
 
 activities <- data.table(activities)
 
